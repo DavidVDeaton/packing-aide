8280b4a10bbf1127622c024e874fa78a3e0f89d8
David Deaton <dideater@gmail.com>
2023-03-29 09:42:19 -0400

    SecurityConfig mostly complete. Still need to finish antMatchers

--- server/packing-aide/src/main/java/learn/easypacking/controllers/AuthController.java
+++ server/packing-aide/src/main/java/learn/easypacking/controllers/AuthController.java
@@ -1,9 +1,22 @@
 package learn.easypacking.controllers;
 
 import learn.easypacking.domain.AppUserService;
+import learn.easypacking.domain.Result;
+import learn.easypacking.models.AppUser;
 import learn.easypacking.security.JwtConverter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class AuthController {
@@ -17,4 +30,60 @@ public class AuthController {
         this.converter = converter;
         this.appUserService = appUserService;
     }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<Map<String, String>> authenticate(@RequestBody Map<String, String> credentials) {
+
+        UsernamePasswordAuthenticationToken authToken =
+                new UsernamePasswordAuthenticationToken(credentials.get("username"), credentials.get("password"));
+
+        try {
+            Authentication authentication = authenticationManager.authenticate(authToken);
+
+            if (authentication.isAuthenticated()) {
+                String jwtToken = converter.getTokenFromUser((AppUser) authentication.getPrincipal());
+
+                HashMap<String, String> map = new HashMap<>();
+                map.put("jwt_token", jwtToken);
+
+                return new ResponseEntity<>(map, HttpStatus.OK);
+            }
+
+        } catch (AuthenticationException ex) {
+            System.out.println(ex);
+        }
+
+        return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+    }
+
+    @PostMapping("/refresh_token")
+    public ResponseEntity<Map<String, String>> refreshToken(@AuthenticationPrincipal AppUser appUser) {
+        String jwtToken = converter.getTokenFromUser(appUser);
+
+        HashMap<String, String> map = new HashMap<>();
+        map.put("jwt_token", jwtToken);
+
+        return new ResponseEntity<>(map, HttpStatus.OK);
+    }
+
+    @PostMapping("/create_account")
+    public ResponseEntity<?> createAccount(@RequestBody Map<String, String> credentials) {
+
+        String username = credentials.get("username");
+        String password = credentials.get("password");
+
+        Result<AppUser> result = appUserService.createUser(username, password);
+
+        // unhappy path...
+        if (!result.isSuccess()) {
+            return new ResponseEntity<>(result.getMessages(), HttpStatus.BAD_REQUEST);
+        }
+
+        // happy path...
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("appUserId", result.getPayload().getAppUserId());
+
+        return new ResponseEntity<>(map, HttpStatus.CREATED);
+    }
 }
+
---
+++ server/packing-aide/src/main/java/learn/easypacking/security/SecurityConfig.java
@@ -0,0 +1,57 @@
+package learn.easypacking.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    private final JwtConverter converter;
+
+    public SecurityConfig(JwtConverter converter) {
+        this.converter = converter;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, AuthenticationConfiguration authConfig) throws Exception {
+
+        http.csrf().disable();
+        http.cors();
+        http.authorizeRequests()
+
+                .antMatchers("/authenticate").permitAll()
+                .antMatchers("/refresh_token").authenticated()
+                .antMatchers("/create_account").permitAll()
+                .antMatchers(HttpMethod.GET,
+                        "/api/container",
+                        "api/container/*",
+                        "api/container/event/*",
+                        "/api/event/*",
+                        "/api/event/user/*",
+                        "/api/item",
+                        "api/item/*",
+                        "api/item/user/*",
+                        "api/item/user/container/*",
+                        "api/location/*",
+                        "api/todo/*").hasAnyAuthority("USER")
+                .antMatchers("/**").denyAll()
+                .and()
+                .addFilter(new JwtRequestFilter(authenticationManager(authConfig), converter))
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+}
+
